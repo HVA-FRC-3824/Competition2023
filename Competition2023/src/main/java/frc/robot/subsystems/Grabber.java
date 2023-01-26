8fb915eb7dc587648241d590fb4f81609d8b0cdd
Joey <joeyjake987@gmail.com>
2023-01-26 12:31:14 -0500

    Compressor pressure on/off

--- Competition2023/src/main/java/frc/robot/subsystems/Grabber.java
+++ Competition2023/src/main/java/frc/robot/subsystems/Grabber.java
@@ -20,8 +20,7 @@ public class Grabber extends SubsystemBase {
     public Grabber(){
         // object constructors
         m_pneumaticHub = new PneumaticHub(Constants.GRABBER_PNEUMATIC_HUB_ID);
-        // m_pneumaticHub.enableCompressorAnalog(100, 120); didn't work, need to try hybrid. If this doesn't work Ill just manually do it with an if statement and getPressure();
-        m_pneumaticHub.enableCompressorDigital(); // enables compressor, should cut off at 120?
+        // m_pneumaticHub.enableCompressorAnalog(100, 120); // didn't work, need to try hybrid. If this doesn't work Ill just manually do it with an if statement and getPressure();
         m_pistonGrabber = new DoubleSolenoid(3, PneumaticsModuleType.REVPH, 2, 3); // Channels TBD, will be constants
     }
 
@@ -29,9 +28,20 @@ public class Grabber extends SubsystemBase {
     @Override
     public void periodic() {
         // Prints out system pressure
-        System.out.println("System Pressure" + m_pneumaticHub.getPressure(0)); // Channels TBD, will be constants
+        // System.out.println("System Pressure" + m_pneumaticHub.getPressure(0)); // Channels TBD, will be constants
         // Puts compressor status on Smart Dashboard
         SmartDashboard.putBoolean("Compressor on: ", m_pneumaticHub.getCompressor());
+        // Puts pressure on the smart dashboard
+        SmartDashboard.putNumber("System Pressure: ", m_pneumaticHub.getPressure(0));
+        
+        // If the Prssure is less than 110 then it enables the compressor and if greater than 120 then it disables the compressor
+        if(m_pneumaticHub.getPressure(0) < 110){
+            // Enables compressor
+            m_pneumaticHub.enableCompressorDigital();
+        }else if(m_pneumaticHub.getPressure(0) > 120){
+            // Disables compressor
+            m_pneumaticHub.disableCompressor();
+        }
     }
 
     /**
