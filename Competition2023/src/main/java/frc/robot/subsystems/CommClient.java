a55c6dcb9a18d6b5495fd998df0867a313ee14fa
bogogion <45zzzpvp@gmail.com>
2023-02-13 10:53:10 -0500

    not finished, started client

--- Competition2023/src/main/java/frc/robot/subsystems/CommClient.java
+++ Competition2023/src/main/java/frc/robot/subsystems/CommClient.java
@@ -1,5 +1,33 @@
 package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class CommClient {
-    /* Test */ 
-}
+import java.io.*;
+import java.net.*;
+
+/* Default connection type is TCP. */
+
+
+public class CommClient extends SubsystemBase{
+
+    /* Create our sockets  */ 
+    private Socket m_socket = null;
+    private DataOutputStream m_recieve = null;
+    
+    String message = "hey from javar"; /* Test message */
+
+    /* Try to establish a connection */
+    public CommClient(String address, int port)
+    {
+        try {
+            m_socket = new Socket(address,port);
+            
+            /* Print on success */
+            System.out.println("Successfully connect to " + address + " on port: " + port);
+
+            /* Init our streams */
+            m_recieve = new DataOutputStream(m_socket.getOutputStream());
+        } catch (Exception i) {
+            System.out.println(i);
+        } 
+    }
+}   
