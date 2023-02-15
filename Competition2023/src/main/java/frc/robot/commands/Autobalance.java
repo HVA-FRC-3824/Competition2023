4866c505f5d6341b0f343a43b040f3ec1cdc81e7
Joey <joeyjake987@gmail.com>
2023-02-15 08:46:59 -0500

    Autobalance command started

--- Competition2023/src/main/java/frc/robot/commands/Autobalance.java
+++ Competition2023/src/main/java/frc/robot/commands/Autobalance.java
@@ -12,13 +12,35 @@ public class Autobalance extends CommandBase {
 
     // Called when the command is initially scheduled.
     @Override
-        public void initialize(){
+    public void initialize(){
+        
     }
   
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute(){
-
+        float tempRollRate = RobotContainer.M_WEST_COAST_DRIVE.returnGyro().getRoll();
+        if(tempRollRate > 1.0){
+            if(tempRollRate > 2.5){
+                if(tempRollRate > 5.0){
+                    // driveWithEncoders(move a lot);
+                }else{
+                    // driveWithEncoders(move a little more);
+                }
+            }else{
+                // driveWithEncoders(move a little);
+            }
+        }else if(tempRollRate < -1.0){
+            if(tempRollRate < -2.5){
+                if(tempRollRate < -5.0){
+                    // driveWithEncoders(-move a lot);
+                }else{
+                    // driveWithEncoders(-move a litte more);
+                }
+            }else{
+                // driveWithEncoders(-move a litte);
+            }
+        }
     }
 
     // Returns true when the command should end.
