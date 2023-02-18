2b7b939dd8544f2d8568e5fde33854693666589f
bogogion <45zzzpvp@gmail.com>
2023-02-17 20:36:04 -0500

    tidy

--- Competition2023/src/main/java/frc/robot/subsystems/communication/TagData.java
+++ Competition2023/src/main/java/frc/robot/subsystems/communication/TagData.java
@@ -3,6 +3,9 @@ package frc.robot.subsystems.communication;
 /* Simple object to hold last known data for tags
  * keep an array of all tags and their ids you want to manage
  * and change info accordingly
+ * 
+ * To access certain data on that tag simply access the array in constants
+ * ie. if the id = 1 then TAG_DATA[id-1];
 */
 
 public class TagData {
