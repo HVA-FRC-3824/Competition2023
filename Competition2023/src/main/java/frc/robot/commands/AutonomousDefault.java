50a9f3452417c37b1914ac12abe90991eb84db5f
zmaples1200 <83434907+bogogion@users.noreply.github.com>
2023-03-08 10:28:50 -0500

    Revert "merge auto in order to have access to updated arm functions"

--- Competition2023/src/main/java/frc/robot/commands/AutonomousDefault.java
+++ Competition2023/src/main/java/frc/robot/commands/AutonomousDefault.java
@@ -1,7 +1,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.RobotContainer;
 
 import java.util.List;
 
@@ -13,26 +12,6 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.RobotContainer;
 
 public class AutonomousDefault extends SequentialCommandGroup{
-
-
-    private AutoScore m_autoScore = new AutoScore();
-
-    public AutonomousDefault(){
-
-        // score
-        m_autoScore.score(0, 0);
-
-        // back up
-        RobotContainer.M_WEST_COAST_DRIVE.drive(-3.2, 0.0);
-
-        // to the cone
-        RobotContainer.M_WEST_COAST_DRIVE.drive(3.2, 0.0);
-
-        // look for april tag
-
-        // pick up an object
-
-
     private Pose2d startingPose = new Pose2d(0, 0, new Rotation2d(0));
     private List<Translation2d> waypoints = List.of(new Translation2d(1.15, 0));
     private Pose2d endingPose = new Pose2d(4.95, 0, new Rotation2d(0));
@@ -40,6 +19,5 @@ public class AutonomousDefault extends SequentialCommandGroup{
         addCommands(
             new InstantCommand(() -> RobotContainer.M_WEST_COAST_DRIVE.generateRamsete(startingPose, waypoints, endingPose, 0, false))
         );
-
     }
 }
